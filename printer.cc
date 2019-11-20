23f64f3ecdeba567d2d27104c417e2250bcf7d9d
harang90 <harang90@gmail.com>
2019-11-20 23:01:55 +0900

    temporary commit 아직 테스트 성공 못함 내일 오전 다시 시도

--- printer.cc
+++ printer.cc
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+bool compare(int a, int b) {
+	return (a < b);
+}
+
 int solution(vector<int> priorities, int location) {
-    int answer = 0;
-    return answer;
+	int answer = 0;
+	vector<int>::iterator iter;
+	iter = priorities.begin();
+	while (1) {
+		if (*iter == *std::max_element(priorities.begin(), priorities.end(), compare)) {
+			answer++;
+			*iter = 0;
+			if (iter - priorities.begin() == location) {
+				break;
+			}
+		}
+		iter++;
+		if (iter == priorities.end()) iter = priorities.begin();
+	}
+	return answer;
 }
 
 int main() {
 	int location = 2;
-	vector<int> priorities{2, 1, 3, 2};
+	vector<int> priorities{1, 1, 9, 1, 1, 1};
 	std::cout << solution(priorities, location) << std::endl;
 }
