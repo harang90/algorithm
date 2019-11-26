111f8723d53dc0fc926383d982ab8eee446c3a8d
harang90 <harang90@gmail.com>
2019-11-26 23:34:22 +0900

    [프로그래머스] 단체사진 찍기

--- Dan-Che-Sa-Jin.cc
+++ Dan-Che-Sa-Jin.cc
@@ -1,12 +1,53 @@
 // [프로그래머스] 단체사진
 
+#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(int n, vector<string> data) {
     int answer = 0;
+		vector<char> line{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+		vector<char>::iterator lineIter, lineIterTwo;
+		do {
+			bool tracker = false;
+			for (int i = 0; i < n; i++) {
+				const char* rule = data[i].c_str();
+				const char from = rule[0];
+				const char to = rule[2];
+				const char exp = rule[3];
+				const char dis = rule[4];
+				for (lineIter = line.begin(); lineIter != line.end(); lineIter++) {
+					if (*lineIter == from) {
+						for (lineIterTwo = line.begin(); lineIterTwo != line.end(); lineIterTwo++) {
+							if (*lineIterTwo == to) break;
+						}
+						int calcDis = std::abs(std::distance(lineIter, lineIterTwo));
+						if (exp == '=') { // 거리가 같음 
+							if (calcDis != dis) {
+								tracker = true;
+								break;
+							}
+						} else if (exp == '>') { // 거리가 미만
+							if (calcDis <= dis) {
+								tracker = true;
+								break;
+							}
+						} else { // 거리가 초과
+							if (calcDis >= dis) {
+								tracker = true;
+								break;
+							}
+						}
+						break;
+					}
+				}
+				if (tracker) break;
+			}
+			if (!tracker) answer++;
+		} while (std::next_permutation(line.begin(), line.end()));
     return answer;
 }
 
@@ -17,6 +58,7 @@ int main () {
 	std::freopen("input.txt", "r", stdin);
 	std::cin >> T;
 	std::vector<string> data;
+	T = 1;
 	for (int i = 0; i < T; i++) {
 //		std::cin >> n;
 //		std::string line;	
@@ -26,6 +68,11 @@ int main () {
 		//라인을 어찌어찌하여 백터로 만들고 등등
 
 		//TEMP
+		//1 <= n <= 100
+		//{A, C, F, J, M, N, R, T}
+		//~
+		//>, =, <
+		//0 ~ 6
 		data.push_back("N~F=0");
 		data.push_back("R~T>2");
 
