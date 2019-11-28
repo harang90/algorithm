f4155f580ac8d8eafa3d83bf7e5b5339ccce9ea0
harang90 <harang90@gmail.com>
2019-11-28 21:11:36 +0900

    [프로그래머스] 숫자의 표현

--- NumberRepresentation.cc
+++ NumberRepresentation.cc
@@ -1,1 +1,34 @@
 // 숫자의 표현
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+		int addNum;
+		int total;
+		for (int i = 1; i <= n; i++) {
+			addNum = i;
+			total = 0;
+			while (1) {
+				total += addNum;	
+				if (total == n) {
+					answer++;
+				}
+				if (total >= n) break;
+				addNum++;
+			}
+		}
+    return answer;
+}
+
+int main() {
+	int T, test_case;
+	freopen("input.txt", "r", stdin);
+	cin >> T;
+	int n;
+	for (test_case = 0; test_case < T; test_case++) {
+		cin >> n;
+		std::cout << solution(n) << std::endl;
+	}
+}
