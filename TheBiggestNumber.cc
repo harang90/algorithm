3baf3e536f3725aefb5565d918fbc2083e36a435
harang90 <harang90@gmail.com>
2019-12-12 08:24:20 +0900

    [프로그래머스] 가장 큰 수 채점 결과 정확성: 36.4 합계: 36.4 / 100.0

--- TheBiggestNumber.cc
+++ TheBiggestNumber.cc
@@ -17,9 +17,9 @@ string solution(vector<int> numbers) {
 		if (*numbers_iter >= 10) {
 			if (*numbers_iter >= 100) {
 				if (*numbers_iter >= 1000) pair_vec.push_back(make_pair(3, *numbers_iter));
-				else pair_vec.push_back(make_pair(2, *numbers_iter * 10));
-			} else pair_vec.push_back(make_pair(1, *numbers_iter * 100));
-		} else pair_vec.push_back(make_pair(0, *numbers_iter * 1000)); 
+				else pair_vec.push_back(make_pair(2, *numbers_iter * 10 + (*numbers_iter / 100) * 1));
+			} else pair_vec.push_back(make_pair(1, *numbers_iter * 100 + (*numbers_iter / 10) * 11));
+		} else pair_vec.push_back(make_pair(0, *numbers_iter * 1000 + *numbers_iter * 111)); 
 	}
 
 	std::sort(pair_vec.begin(), pair_vec.end(), [](const pair<int,int> & a, pair<int,int> & b) -> bool {
@@ -29,9 +29,9 @@ string solution(vector<int> numbers) {
 	vector<pair<int,int>>::iterator pair_vec_iter;
 	for (pair_vec_iter = pair_vec.begin(); pair_vec_iter != pair_vec.end(); pair_vec_iter++) {
 		int element;
-		if (pair_vec_iter->first == 0) element = pair_vec_iter->second / 1000;
-		else if (pair_vec_iter->first == 1) element = pair_vec_iter->second / 100;
-		else if (pair_vec_iter->first == 2) element = pair_vec_iter->second / 10;
+		if (pair_vec_iter->first == 0) element = (pair_vec_iter->second - ((pair_vec_iter->second / 1000) * 111)) / 1000;
+		else if (pair_vec_iter->first == 1) element = (pair_vec_iter->second - ((pair_vec_iter->second / 1000) * 11)) / 100;
+		else if (pair_vec_iter->first == 2) element = (pair_vec_iter->second - (pair_vec_iter->second / 1000)) / 10;
 		else element = pair_vec_iter->second;
 		answer.append(to_string(element));
 	}
@@ -39,6 +39,6 @@ string solution(vector<int> numbers) {
 }
 
 int main() {
-	vector<int> numbers{3, 30, 34, 5, 9};
+	vector<int> numbers{4, 45, 446, 5, 9};
 	std::cout << solution(numbers) << std::endl;
 }
