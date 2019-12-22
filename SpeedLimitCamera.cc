356d6328a0975e1058febdeb40fbae2ac7f1f668
harang90 <harang90@gmail.com>
2019-12-22 20:06:01 +0900

    [프로그래머스] 단속 카메라 채점 결과 정확성: 0.0 효율성: 0.0 합계: 0.0 / 100.0

--- SpeedLimitCamera.cc
+++ SpeedLimitCamera.cc
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -9,11 +10,36 @@ int solution(vector<vector<int>> routes) {
 	// routes가 0이 되면 return;
 
 	int answer{0};
+	int max;
+	int max_i;
+	int max_j;
+	int num;
 	while (routes.size()) {
+		max = 0;
+		max_i = -1;
+		max_j = -1;
 		for (int i = 0; i < routes.size(); i++) {
-			for
-			routes[i][0];
+			for (int j = 0; j < 2; j++) {
+				num = 0;
+				for (int k = 0; k < routes.size(); k++) {
+					if ((routes[k][0] <= routes[i][j]) && (routes[i][j] <= routes[k][1])) {
+						num++;
+					}
+				}
+				if (num > max) {
+					max = num;
+					max_i = i;
+					max_j = j;
+				}
+			}
 		}
+		for (int k = 0; k < routes.size(); k++) {
+			if ((routes[k][0] <= routes[max_i][max_j]) && (routes[max_i][max_j] <= routes[k][1])) {
+				routes.erase(routes.begin() + k);
+				k--;
+			}
+		}
+		answer++;
 	}
 	return answer;
 }
