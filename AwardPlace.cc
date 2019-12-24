7f7eeedf4cb0a936896baa845a9777530a5b5c0c
taisun_park <taisun_park@tmax.co.kr>
2019-12-24 11:00:25 +0900

    [프로그래머스] 순위

---
+++ AwardPlace.cc
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+	return 1;
+}
+
+int main() {
+	return EXIT_SUCCESS;
+}
